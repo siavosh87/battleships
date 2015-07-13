4c4746626e0febe5d384e8b9f0962f11cea0c0a2
siavosh87 <siavosh.mahboubian@gmail.com>
2015-07-13 17:02:41 +0100

    Building tabels with for loop as shown by Alex

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-function BattleShipGame(){
-}
+// Setup the two boards
+// Choose the positions where you want your battle ships to go.
+// Make the computer choose the positions of their battleships (AI)
+// You choose to hit one of their squares
+// Save the choice of square in an array
+// Make the computer make a random choice of squares (primative AI)
+// Then work out when the game is finished
 
+var level = 10,
 
-function Ship(name, size) {
-  this.name = name;
-  this.size = size;
-}
+buildTables = function(side){
+  $('body').append("<ul class='"+side+"''></ul>")
+  var $grid = $('.'+side);
+  var i = 0;
+  for (i; i < (level*level); i++) {
+    $grid.append("<li data-value='"+side+"-"+i+"'></li>");
+  }
+},
 
-// if 'new game' is pressed:
-function newGame() {
+start = function() {
+  buildTables("left");
+  buildTables("right");
+};
 
-}
+$(function(){
+  start();
+})
